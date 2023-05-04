75174a4b284f65443256db9f4538f2145e0d95c8
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2023-05-04 16:57:12 +0900

    gitignore

--- Diary/Diary/DiaryDetailViewController.swift
+++ Diary/Diary/DiaryDetailViewController.swift
@@ -42,11 +42,24 @@ class DiaryDetailViewController: UIViewController {
         return formatter.string(from: date)
     }
     
+    @objc func editDiaryNotification(_ notification: Notification) { // 수정된 다이어리 객체를 전달받아서 뷰에 업데이트하기.
+        guard let diary = notification.object as? Diary else { return }  // Diary 타입으로 다운 캐스팅.
+        // 포스트 할 때, userInfo의 indexPath.row값을 딕셔너리로 보내는데, 이것도 가져와 보겠음.
+        guard let row = notification.userInfo?["indexPath.row"] as? Int else { return }
+        self.diary = diary  // 수정된 diary(전달 받은 거)객체를 대입
+        self.configureView()  // 수정된 것이 View가 업데이트되게 만듦.
+    }
+    
     @IBAction func tapEditButton(_ sender: UIButton) {
         guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "WriteDiaryViewController") as? WriteDiaryViewController else { return }
         guard let indexPath = self.indexPath else { return }
         guard let diary = self.diary else { return }
         viewController.diaryEditroMode = .edit(indexPath, diary)
+        // notification옵저빙
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(editDiaryNotification(_:)),
+                                               name: NSNotification.Name("editDiary"),
+                                               object: nil)
         self.navigationController?.pushViewController(viewController, animated: true)
     }
     
@@ -55,6 +68,10 @@ class DiaryDetailViewController: UIViewController {
         self.delegate?.didSelectDelete(indexPath: indexPath)
         self.navigationController?.popViewController(animated: true)
 
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)  // 해당 인스턴스에 추가된 옵저버가 모두 삭제되게...
     }
     
 }
--- Diary/Diary/WriteDiaryViewController.swift
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -90,7 +90,16 @@ class WriteDiaryViewController: UIViewController {
         guard let contents = self.contentsTextView.text else { return }
         guard let date = self.diaryDate else { return }
         let diary = Diary(title: title, contents: contents, date: date, isStar: false)
-        self.delegate?.didSelectRegister(diary: diary)
+        
+        switch self.diaryEditroMode {
+        case .new:
+            self.delegate?.didSelectRegister(diary: diary)
+            
+        case let .edit(indexPath, _):
+            NotificationCenter.default.post(name: NSNotification.Name("editDary"),
+                                            object: diary,  // 수정된 내용의 diary
+                                            userInfo: ["indexPath.row": indexPath.row])
+        }
         self.navigationController?.popViewController(animated: true)
     }
     
